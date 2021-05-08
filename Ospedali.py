b1fd93a3d0ae3575121137f2363ceac2f74acbbe
annabavaresco <74197386+annabavaresco@users.noreply.github.com>
2021-05-08 12:02:11 +0200

    Aggiunti 2 file
    Ho aggiunto alla cartella i file Ospedali.py e Prova_progetto.py
    Il primo contiene la classe ospedale e le funzioni per passare dai dizionari del json agli oggetti della classe ospedale.
    
    Il file Prova_progetto.py contiene il codice per salvare i dati degli ospedali nel db di Amazon RDS

---
+++ Ospedali.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+
+
+
+class InGestione:
+    def __init__(self, arancio: int, azzurro: int, bianco: int, giallo: int, rosso: int, verde: int):
+        self.arancio = arancio
+        self.azzurro = azzurro
+        self.bianco = bianco
+        self.giallo = giallo
+        self.rosso = rosso
+        self.verde = verde
+
+class InAttesa:
+    def __init__(self, arancio: int, azzurro: int, bianco: int, giallo: int, rosso: int, verde: int):
+        self.arancio = arancio
+        self.azzurro = azzurro
+        self.bianco = bianco
+        self.giallo = giallo
+        self.rosso = rosso
+        self.verde = verde
+
+class Hospital:
+
+    def __init__(self, code: str, in_gestione: InGestione, in_attesa: InAttesa, timestamp):
+        
+        self.in_attesa = in_attesa
+        self.codice = code
+        self.in_gestione = in_gestione
+        self.timestamp = timestamp
+
+
+
+def from_dict_to_hosp(ospedale):
+    att = InAttesa(int(ospedale['risposta']['pronto_soccorso']['reparto']['attesa']['arancio']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['attesa']['azzurro']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['attesa']['bianco']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['attesa']['giallo']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['attesa']['rosso']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['attesa']['verde'])) 
+
+    
+    gest = InGestione(int(ospedale['risposta']['pronto_soccorso']['reparto']['ambulatorio']['arancio'])+
+                     int(ospedale['risposta']['pronto_soccorso']['reparto']['osservazione']['arancio']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['ambulatorio']['azzurro'])+
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['osservazione']['azzurro']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['ambulatorio']['bianco'])+
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['osservazione']['bianco']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['ambulatorio']['giallo'])+
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['osservazione']['giallo']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['ambulatorio']['rosso'])+
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['osservazione']['rosso']),
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['ambulatorio']['verde'])+
+                    int(ospedale['risposta']['pronto_soccorso']['reparto']['osservazione']['verde']))  
+
+
+    ret = Hospital(ospedale['risposta']['pronto_soccorso']['reparto']['codice'],
+                    gest,
+                    att,
+                    datetime.strptime(ospedale['risposta']['timestamp'].replace(' ore',''), "%d/%m/%Y %H:%M"),         
+    )
+    return ret
+
+def from_lod_to_los(lista_di_dizionari):
+    ret = []
+    for dizionario in lista_di_dizionari:
+        ret.append(from_dict_to_hosp(dizionario))
+    return ret
+
+
---
+++ Prova_progetto.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import time
+from pprint import pprint
+from Ospedali import *
+import mysql.connector 
+
+resp = requests.get('https://servizi.apss.tn.it/opendata/STATOPS001.json')
+hosp = resp.json()
+
+ospedali = from_lod_to_los(hosp)
+
+connection = mysql.connector.connect(
+    host = 'emergencyroom.ci8zphg60wmc.us-east-2.rds.amazonaws.com',
+    port =  3306,
+    user = 'admin',
+    database = 'prova',
+    password = 'emr00mtr3nt036'
+)
+
+connection.autocommit = True
+cursor = connection.cursor()
+
+query = "insert into ers (timestamp, codice_ospedale, bianco_attesa,\
+    bianco_gestione, giallo_attesa, giallo_gestione, verde_attesa,\
+        verde_gestione, azzurro_attesa, azzurro_gestione, arancio_attesa,\
+            arancio_gestione, rosso_attesa, rosso_gestione)\
+                values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+for ospedale in ospedali:
+    cursor.execute(query, [ospedale.timestamp, ospedale.codice, ospedale.in_attesa.bianco,\
+        ospedale.in_gestione.bianco, ospedale.in_attesa.giallo, ospedale.in_gestione.giallo,\
+             ospedale.in_attesa.verde, ospedale.in_gestione.verde, ospedale.in_attesa.azzurro,\
+                 ospedale.in_gestione.azzurro, ospedale.in_attesa.arancio, ospedale.in_gestione.arancio,\
+                     ospedale.in_attesa.rosso, ospedale.in_gestione.rosso])
+connection.close()
+
+
